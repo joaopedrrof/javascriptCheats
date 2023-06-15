9f917be9f6dbfe99035ab4d37c7d80081c17742c
João Pedro Ferreira <joaopedrro12@gmail.com>
2023-06-15 10:34:03 +0200

    Update parseArraygit.js

--- parseArraygit.js
+++ parseArraygit.js
@@ -1,4 +1,3 @@
 let jsonString = '{"start_date":1677024000000,"my_object_id":34345,"first_delivery_date":1677456000000}'
 let myArray = JSON.parse(jsonString);
-// output = [{value: myArray}];
 console.log([{value: myArray}])
