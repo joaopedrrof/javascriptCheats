02b9d5d6d2090f5fbefdaf5c4b4484dcc200273b
João Pedro Ferreira <joaopedrro12@gmail.com>
2023-06-18 13:25:46 +0200

    Update occurence-counter.js

--- occurence-counter.js
+++ occurence-counter.js
@@ -1,7 +1,5 @@
-// WIP
-let pipelines ='{1677,34345,}'
-const pipelinesIds = pipelines.split(',');
-const target = "9903661"; 
+let pipelines = ['1677','34345','9903661','9903661'];
+const target = "9903661";
 
-const onboardingCount = pipelinesIds.filter(id => id === target).length
-console.log([{value:onboardingCount}])
+const onboardingCount = pipelines.filter(id => id === target).length;
+console.log([{ target_occurence: onboardingCount }]);
